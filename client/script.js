d5816e41c7103e3a7a96b6c237a33cde4b6babb1
Huy LQ <lqhuy.it@gmail.com>
2023-01-13 11:18:22 +0700

    Change server URL

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   //fetch data from server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://gptchat-06m2.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
